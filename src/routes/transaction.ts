09b9ba75523784ec38ddbf3ea027689026eb0888
Sam-2019 <samtmartey@gmail.com>
2025-11-14 19:48:21 +0000

    fix

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -3,6 +3,6 @@ import { authMiddleware } from "@/middlewares/auth";
 import { createTransaction } from "@/controllers/transactions";
 
 const router = express.Router();
-router.route("/trnasaction").post(authMiddleware, createTransaction);
+router.route("/transaction").post(authMiddleware, createTransaction);
 
 export { router as saleRouter };
